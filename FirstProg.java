9a35b261a6c325784e61a64f1463004c3945da92
LJ <1johnlekkas2@gmail.com>
2023-10-30 19:56:38 +0200

    Add files via upload

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+	public static void main( String[] arg )
+	{
+		System.out.println( "I, John Lekkas, am determined to learn how to code." );
+		System.out.println( "Today's date is Monday, October 30, 2023." );
+	}
+}
