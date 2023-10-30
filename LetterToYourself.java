f6abf9191067e7348d75b40acc4ff6a3e60da3bc
LJ <1johnlekkas2@gmail.com>
2023-10-30 19:56:49 +0200

    Add files via upload

---
+++ LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("+-------------------------------------------------------------------+");
+		System.out.println("|                                                              #### |");
+		System.out.println("|                                                              #### |");
+		System.out.println("|                                                              #### |");
+		System.out.println("|                                                                   |");
+		System.out.println("|                                                                   |");
+		System.out.println("|                                        John Lekkas                |");
+		System.out.println("|                                        1A Silicon Vallway         |");
+		System.out.println("|                                        Sealand, 10101             |");
+		System.out.println("|                                                                   |");
+		System.out.println("+-------------------------------------------------------------------+");
+	}
+}
