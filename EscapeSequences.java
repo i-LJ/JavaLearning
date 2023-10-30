b7adc7419312745d48faae5d3ce4f28ecd1241e5
LJ <1johnlekkas2@gmail.com>
2023-10-30 19:57:11 +0200

    Add files via upload

---
+++ EscapeSequences.java
@@ -0,0 +1,16 @@
+public class EscapeSequences
+{
+	public static void main( String[] args )
+	{
+		// Initial version created uing FIGlet, font "Big Money", oriented southwest
+
+		System.out.print( "\t    ______\n\t    /     |\n\t    JJJJJ |" );
+		System.out.println( "  ______   __     __  _______" );
+		System.out.println( "\t       JJ | /      \\ /  \\   /  |/       \\" );
+		System.out.println( "\t __    JJ | aaaaaa  |\"\"  \\ /\"\"/ aaaaaa  |" );
+		System.out.println( "\t/  |   JJ | /    aa | \"\"  /\"\"/  /    aa |" );
+		System.out.println( "\tJJ \\___JJ |/aaaaaaa |  \"\" \"\"/  /aaaaaaa |" );
+		System.out.println( "\tJJ     JJ/ aa    aa |   \"\"\"/   aa    aa |" );
+		System.out.println( "\t  JJJJJJ/   aaaaaaa/     \"/     aaaaaaa/" );
+	}
+}
